34356560a101fcf545773d9387f415adb100f3f2
Daniel Nguyen <danielnguyenh07@gmail.com>
2026-09-04 16:58:17 +0800

    Layout all the required function for lab1

---
+++ lab1.py
@@ -0,0 +1,17 @@
+'''
+SC2001 Lab Project 1: Integration of Merge Sort & Insertion Sort
+
+'''
+
+def insertion_sort(arr):
+    pass
+
+def merge_sort(arr):
+    pass
+
+# Merge function to support merge sort and hybrid sort
+def merge(arr):
+    pass
+
+def hybrid_sort(arr):
+    pass

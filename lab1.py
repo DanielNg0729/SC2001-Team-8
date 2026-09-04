0d1bab228e3c74bdfdeb636747a68ac256d68568
Daniel Nguyen <danielnguyenh07@gmail.com>
2026-09-04 17:07:56 +0800

    complete insertion sort

--- lab1.py
+++ lab1.py
@@ -3,9 +3,22 @@ SC2001 Lab Project 1: Integration of Merge Sort & Insertion Sort
 
 '''
 
-def insertion_sort(arr):
-    pass
-
+def insertion_sort(arr, low, high):
+    # Sort arr[low:high]
+    count = 0
+    for i in range(low + 1, high + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= low:
+            count += 1 
+            if arr[j] > key:
+                arr[j+1] = arr[j]
+                j -= 1
+            else:
+                break
+        arr[j + 1] = key
+    return count, arr
+        
 def merge_sort(arr):
     pass
 
@@ -15,3 +28,4 @@ def merge(arr):
 
 def hybrid_sort(arr):
     pass
+

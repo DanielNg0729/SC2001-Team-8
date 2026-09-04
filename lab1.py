06c761a089265140e5a661df0511514cebd2e34d
Daniel Nguyen <danielnguyenh07@gmail.com>
2026-09-04 17:30:41 +0800

    Complete Merge function and Merge sort

--- lab1.py
+++ lab1.py
@@ -19,12 +19,46 @@ def insertion_sort(arr, low, high):
         arr[j + 1] = key
     return count, arr
         
-def merge_sort(arr):
-    pass
 
 # Merge function to support merge sort and hybrid sort
-def merge(arr):
-    pass
+def merge(arr, low, mid,high):
+    left, right = arr[low: mid + 1], arr[mid + 1: high]
+    count = 0
+    i,j = 0,0
+    k = low
+    nl, nr = len(left), len(right)
+    while i < nl and j < nr:
+        count += 1
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+    while i < nl:
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while j < nr:
+        arr[k] = right[j]
+        j += 1
+        k += 1
+    return count, arr
+
+def merge_sort(arr, low, high):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(arr) - 1
+    if high <= low:
+        return 0
+    mid = (low + high) // 2
+    c = merge_sort(arr,low,high)
+    c += merge_sort(arr,mid+1,high)
+    c += merge(arr, low, mid, high)
+    return c
+
 
 def hybrid_sort(arr):
     pass
